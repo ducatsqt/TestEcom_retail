d1f10ff8a1bd44efcaeb96164406930918f982e9
scs <scs@192.168.1.7>
2023-11-04 15:12:53 +0530

    This is my first commit for Retail e-com application

---
+++ src/TestingEComProject/TstingProductsHomePage.java
@@ -0,0 +1,25 @@
+package TestingEComProject;
+
+public class TstingProductsHomePage {
+
+	
+		// use app UI to create test scripts
+		
+		public void test1()
+		{
+			System.out.println("testHomepage");
+		}
+		
+		public void test2()
+		{
+			System.out.println("testLoginPage");
+		}
+		
+		public void test3()
+		{
+			System.out.println("Test product search");
+		}
+
+	
+
+}
